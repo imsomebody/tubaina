ded4f2ca6e30f625efb0226431d78caaea9d49f7
Davi Viana <davianasales@outlook.com>
2022-09-09 17:40:49 -0300

    tubaina(encoder): create QRCode translator

---
+++ Tubaina.Encoder/QR.cs
@@ -0,0 +1,27 @@
+﻿using QRCoder;
+using QRCoder.Extensions;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubaina.Encoder
+{
+    /// <summary>
+    /// Fornece ferramentas para a conversão de strings de dados em Códigos QR.
+    /// </summary>
+    internal class QR
+    {
+        public static string GetBase64(string data)
+        {
+            using(var qrGenerator = new QRCodeGenerator())
+            using(var qrCode = qrGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.Q))
+            using(var qrBmp = new BitmapByteQRCode(qrCode))
+            {
+                return Convert.ToBase64String(qrBmp.GetGraphic(20));
+            }
+        }
+    }
+}
