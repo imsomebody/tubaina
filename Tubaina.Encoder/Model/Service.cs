3c5136af2dd83bc07603c8227e5e38deffc3b416
Davi Viana <davianasales@outlook.com>
2022-09-09 17:38:09 -0300

    tubaina(encoder): service model

---
+++ Tubaina.Encoder/Model/Service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubaina.Encoder.Model
+{
+    public class ServiceRequest
+    {
+        public string Identification { get; set; }
+        public string Information { get; set; }
+        public string File { get; set; }
+    }
+
+    public class Service
+    {
+        public string Uri { get; set; }
+        public object File { get; set; }
+    }
+}
