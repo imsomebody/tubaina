d2dd27ec04379e16bd865ca1492f825192c58a1c
Davi Viana <davianasales@outlook.com>
2022-09-09 17:41:09 -0300

    tubaina(encoder): create replacer

---
+++ Tubaina.Encoder/HTML/Replacer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubaina.Encoder.HTML
+{
+    public static class Replacer
+    {
+        public static string Replace((string, string)[] tokens, string value)
+        {
+            var replaceableValue = value;
+
+            foreach(var token in tokens)
+            {
+                replaceableValue = replaceableValue.Replace($"{{{{ {token.Item1} }}}}", token.Item2);
+            }
+
+            return replaceableValue;
+        }
+    }
+}
