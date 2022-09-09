981708d29e5817c0640d3595f46f76c35bf5aca5
Davi Viana <davianasales@outlook.com>
2022-09-09 17:38:23 -0300

    tubaina(entry): add DinkToPdf injection

--- Tubaina/Program.cs
+++ Tubaina/Program.cs
@@ -1,11 +1,13 @@
+using DinkToPdf;
+using DinkToPdf.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
