7acb77c69ef038c1a5198004bc76627ce27a2f7d
Davi Viana <davianasales@outlook.com>
2022-09-09 17:38:54 -0300

    tubaina(encoder-crypt): create module

---
+++ Tubaina.Encoder/Crypt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubaina.Encoder
+{
+    public static class Crypt
+    {
+        public static String CreateSha256(String value)
+        {
+            StringBuilder Sb = new StringBuilder();
+
+            using (SHA256 hash = SHA256.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                Byte[] result = hash.ComputeHash(enc.GetBytes(value));
+
+                foreach (Byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+
+            return Sb.ToString();
+        }
+    }
+}
